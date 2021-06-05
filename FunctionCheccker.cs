6f199d6bebb0772e16b301439220aa57f037b1ee
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-06-05 20:39:08 +0200

    Add project files.

---
+++ FunctionCheccker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POK_CW2_ZAD2
+{
+    public class FunctionCheccker
+    {
+        private const int cst = 2;
+        public FunctionCheccker(Function f1)
+        {
+
+            F1 = f1;
+            Fconst = new Function(F1.paramA, F1.paramB, cst);
+        }
+
+        public Function F1 { get; set; }
+        public Function Fconst { get; set; }
+
+        public bool CheckCorrect()
+        {
+
+            var randomTab = new List<int>();
+            randomTab = FillTable(10);
+            bool result = false;
+            foreach (var i in randomTab)
+            {
+                result = CheckRequairments(i);
+                if (result == false)
+                {
+                    return false;
+                }
+            }
+            return result;
+        }
+
+        private bool CheckRequairments(int i)
+        {
+            F1.paramX = i;
+            var test = (F1.paramY - Fconst.paramY) / (i - Fconst.paramX);
+            if (test == F1.paramA)
+            {
+                return true;
+            }
+            else return false;
+        }
+        private List<int> FillTable(int v)
+        {
+            var list = new List<int>();
+            var r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(r.Next(v));
+            }
+            return list;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace POK_CW2_ZAD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var function = new Function(4, 1);
+            var fcheck = new FunctionCheccker(function);
+
+            var result =  fcheck.CheckCorrect();
+            if(result == true)
+            {
+                Console.WriteLine(function.ToString() + " Jest poprawna");
+            }
+            else
+            {
+                Console.WriteLine(function.ToString() + " Jest niepoprawna");
+            }
+            
+
+
+        }
+    }
+    public class Function
+    {
+        public Function()
+        {
+
+        }
+
+        public Function(float paramA, float paramB)
+        {
+            this.paramA = paramA;
+            this.paramB = paramB;
+        }
+
+        public Function(float paramA, float paramB, float paramX)
+        {
+            this.paramA = paramA;
+            this.paramB = paramB;
+            this.paramX = paramX;
+        }
+
+        public float paramA { get; set; }
+
+        public float paramB { get; set; }
+
+        public float? paramX { get; set; }
+
+        public float? paramY { get =>paramX != null? paramA * paramX + paramB : null; }
+
+
+
+       
+
+       
+        public override string ToString()
+        {
+            var str = "";
+            if (paramB > 0)
+            {
+                str = $"f(x) = {paramA}x + {paramB}";
+            }
+            else if (paramB == 0)
+            {
+                str = $"f(x) = {paramA}x";
+            } else
+            {
+                str = $"f(x) = {paramA}x - {paramB}";
+            }
+
+            return str;
+
+        }
+
+
+    }
+}
